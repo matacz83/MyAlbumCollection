291e926e6903b482e0b6aa0626dba72c53398600
matacz <matacz@gmail.com>
2019-08-03 12:04:38 +0200

    Initial commit

---
+++ src/main/java/pl/sda/jsp/Album.java
@@ -0,0 +1,44 @@
+package pl.sda.jsp;
+
+public class Album {
+
+    private String title;
+    private String artist;
+    private String genre;
+    private int year;
+
+    public Album() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,30 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: matacz
+  Date: 03.08.2019
+  Time: 11:33
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>My Albums</title>
+</head>
+<body>
+
+<form method="post" action="/">
+    Title:<br><input type="text" name="title"><br>
+    Artist:<br><input type="text" name="artist"><br>
+    Genre:<br><input type="text" name="genre"><br>
+    Year:<br><input type="text" name="year"><br>
+    <input type="submit">
+</form>
+
+<%
+    
+
+%>
+
+
+</body>
+</html>
